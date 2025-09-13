7b870f3fdaaae5e285a17aa2dbadb322c533e300
csae8092 <p.andorfer@gmail.com>
2025-09-13 10:44:24 +0200

    feat: write concepts as terms into back element of edition files #2

--- pyscripts/denormalize_concepts.py
+++ pyscripts/denormalize_concepts.py
@@ -3,8 +3,11 @@ import os
 
 import lxml.etree as ET
 from acdh_tei_pyutils.tei import TeiReader
-from rdflib import RDF, SKOS, Graph
+from acdh_tei_pyutils.utils import check_for_hash
+from rdflib import RDF, SKOS, Graph, Literal
 from tqdm import tqdm
+
+print("write skos:Concepts as tei:term into back element")
 
 files = sorted(glob.glob("./data/editions/*xml"))
 domain = "https://vocabs.acdh.oeaw.ac.at/traveldigital/"
@@ -19,30 +22,35 @@ for s, p, o in g.triples((None, RDF.type, SKOS.Concept)):
 
 concept_info = {}
 for concept in concepts:
-    prefLabel = g.value(subject=concept, predicate=SKOS.prefLabel)
-    definition = g.value(subject=concept, predicate=SKOS.definition)
+    prefLabel = None
+    for label in g.objects(subject=concept, predicate=SKOS.prefLabel):
+        if isinstance(label, Literal) and label.language == "de":
+            prefLabel = label
+            break
 
-    # Get broader concepts with their labels
+    definition = None
+    for d in g.objects(subject=concept, predicate=SKOS.definition):
+        if isinstance(d, Literal) and d.language == "de":
+            definition = d
+            break
+
+    # Get broader concepts with their labels; would bloat the TEIs for very little added value
     broader_concepts = []
-    for obj in g.objects(subject=concept, predicate=SKOS.broader):
-        broader_label = g.value(subject=obj, predicate=SKOS.prefLabel)
-        broader_concepts.append(
-            {
-                "uri": str(obj),
-                "label": str(broader_label) if broader_label else str(obj),
-            }
-        )
+    # for obj in g.objects(subject=concept, predicate=SKOS.broader):
+    #     broader_label = g.value(subject=obj, predicate=SKOS.prefLabel, lang="de")
+    #     broader_concepts.append({
+    #         'uri': str(obj),
+    #         'label': str(broader_label) if broader_label else str(obj)
+    #     })
 
     # Get narrower concepts with their labels
     narrower_concepts = []
-    for obj in g.objects(subject=concept, predicate=SKOS.narrower):
-        narrower_label = g.value(subject=obj, predicate=SKOS.prefLabel)
-        narrower_concepts.append(
-            {
-                "uri": str(obj),
-                "label": str(narrower_label) if narrower_label else str(obj),
-            }
-        )
+    # for obj in g.objects(subject=concept, predicate=SKOS.narrower):
+    #     narrower_label = g.value(subject=obj, predicate=SKOS.prefLabel, lang="de")
+    #     narrower_concepts.append({
+    #         'uri': str(obj),
+    #         'label': str(narrower_label) if narrower_label else str(obj)
+    #     })
 
     key = str(concept)
     value = {
@@ -53,51 +61,72 @@ for concept in concepts:
     }
     item = ET.Element(
         "{http://www.tei-c.org/ns/1.0}item",
-        attrib={
-            "{http://www.w3.org/XML/1998/namespace}id": key,
-            "n": value["prefLabel"],
-        },
+        attrib={"corresp": key.replace(domain, "t:"), "n": value["prefLabel"]},
     )
     term = ET.SubElement(item, "{http://www.tei-c.org/ns/1.0}term")
     term.text = value["prefLabel"]
-    note = ET.SubElement(item, "{http://www.tei-c.org/ns/1.0}note")
+    note = ET.SubElement(
+        item, "{http://www.tei-c.org/ns/1.0}note", attrib={"type": "definition"}
+    )
     note.text = (
         value["definition"] if value["definition"] else "No definition available."
     )
-    if value["broader"]:
-        list_relation = ET.SubElement(
-            item,
-            "{http://www.tei-c.org/ns/1.0}listRelation",
-            attrib={"type": "broader"},
-        )
-        for broader in value["broader"]:
-            relation = ET.SubElement(
-                list_relation,
-                "{http://www.tei-c.org/ns/1.0}relation",
-                attrib={
-                    "active": key,
-                    "passive": broader["uri"],
-                    "n": broader["label"],
-                },
-            )
-    if value["narrower"]:
-        list_relation = ET.SubElement(
-            item,
-            "{http://www.tei-c.org/ns/1.0}listRelation",
-            attrib={"type": "narrower"},
-        )
-        for narrower in value["narrower"]:
-            relation = ET.SubElement(
-                list_relation,
-                "{http://www.tei-c.org/ns/1.0}relation",
-                attrib={
-                    "active": key,
-                    "passive": narrower["uri"],
-                    "n": narrower["label"],
-                },
-            )
+    # if value["broader"]:
+    #     list_relation = ET.SubElement(
+    #         item,
+    #         "{http://www.tei-c.org/ns/1.0}listRelation",
+    #         attrib={"type": "broader"},
+    #     )
+    #     for broader in value["broader"]:
+    #         relation = ET.SubElement(
+    #             list_relation,
+    #             "{http://www.tei-c.org/ns/1.0}relation",
+    #             attrib={
+    #                 "active": key,
+    #                 "passive": broader["uri"],
+    #                 "n": broader["label"],
+    #             },
+    #         )
+    # if value["narrower"]:
+    #     list_relation = ET.SubElement(
+    #         item,
+    #         "{http://www.tei-c.org/ns/1.0}listRelation",
+    #         attrib={"type": "narrower"},
+    #     )
+    #     for narrower in value["narrower"]:
+    #         relation = ET.SubElement(
+    #             list_relation,
+    #             "{http://www.tei-c.org/ns/1.0}relation",
+    #             attrib={
+    #                 "active": key,
+    #                 "passive": narrower["uri"],
+    #                 "n": narrower["label"],
+    #             },
+    #         )
     value["tei"] = item
     concept_info[key] = value
 
 for x in tqdm(files):
     doc = TeiReader(x)
+    text_node = doc.any_xpath(".//tei:text")[0]
+    for back in doc.any_xpath(".//tei:back"):
+        back.getparent().remove(back)
+    back_node = ET.SubElement(text_node, "{http://www.tei-c.org/ns/1.0}back")
+    skos_refs = set()
+    for ref in doc.any_xpath(".//tei:rs[@ref]/@ref"):
+        for skos_id in ref.split():
+            skos_uri = check_for_hash(skos_id).replace("t:", domain)
+            skos_refs.add(skos_uri)
+    if skos_refs:
+        term_list = ET.SubElement(
+            back_node, "{http://www.tei-c.org/ns/1.0}list", attrib={"type": "concepts"}
+        )
+        for ref in skos_refs:
+            try:
+                match = concept_info[ref]
+            except KeyError:
+                continue
+            term_list.append(match["tei"])
+    doc.tree_to_file(x)
+
+print("done")
--- shellscripts/process.sh
+++ shellscripts/process.sh
@@ -2,3 +2,4 @@
 
 echo "adding xml:ids"
 uv run add-attributes -g "data/editions/*.xml" -b "https://baedeker-digital.acdh.oeaw.ac.at"
+uv run pyscripts/denormalize_concepts.py
