e997781406e4c7b41e4fbf783236758ed089fa9c
csae8092 <p.andorfer@gmail.com>
2025-11-27 16:26:09 +0100

    feat: denormlize mentioned persons

--- shellscripts/process.sh
+++ shellscripts/process.sh
@@ -4,3 +4,5 @@ echo "adding xml:ids"
 uv run add-attributes -g "data/editions/*.xml" -b "https://baedeker-digital.acdh.oeaw.ac.at"
 uv run pyscripts/remove_new_lines.py
 uv run pyscripts/denormalize_concepts.py
+
+uv run denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/listperson*.xml" -m ".//tei:persName/@ref" -x ".//tei:titleStmt/tei:title[@level='a'][1]/text()"
